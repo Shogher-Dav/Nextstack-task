2184ea021fef90d957b2e41d3b69a132dbba5e84
Shogher <shogher@Shoghs-MacBook-Pro.local>
2024-05-03 18:50:36 +0400

    change main file, remove bug from app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
-import express, { Express, Request, Response, json } from "express";
-import bodyParser from "body-parser";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import connectDB from "./config/db";
+import express, { Express, Request, Response, json } from 'express';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import connectDB from './config/db';
 
-import { products } from "./recources/products";
-import { orders } from "./recources/orders";
-import { errorHandler } from "./middlewares/errors";
-import { user } from "./recources/users";
+import { products } from './recources/products';
+import { orders } from './recources/orders';
+import { errorHandler } from './middlewares/errors';
+import { user } from './recources/users';
 
 const app: Express = express();
 
@@ -20,14 +20,14 @@ app.use(cors());
 
 connectDB();
 
-app.use("/products", products);
-app.use("/orders", orders);
-app.use("/user", user);
-app.use(errorHandler);
+app.use('/products', products);
+app.use('/orders', orders);
+app.use('/user', user);
 
-app.get("/", (_: Request, res: Response) => {
-  res.send("Hello from Server!");
+app.get('/', (_: Request, res: Response) => {
+  res.send('Hello from Server!');
 });
+app.use(errorHandler);
 
 const port = process.env.PORT || 3000;
 
